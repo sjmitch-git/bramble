066a11ffa880da8cc4d2e5981c5393a55d3b11e1
sjmitch-git <sjmitch20@outlook.com>
2023-06-12 22:52:27 +0100

    Modal

--- templates/Modal.tsx
+++ templates/Modal.tsx
@@ -21,19 +21,23 @@ const ModalTemplate = () => {
 
 	const handleClick = () => {
 		setModal(true)
+		document.body.style.overflow = 'hidden'
 	}
 
 	const closeModal = () => {
 		setModal(false)
+		document.body.style.overflow = ''
 	}
 
 	return (
 		<>
 			<h2>Usage {modal}</h2>
 
-			<p>Click on image to open Modal</p>
+			<p>
+				Click on image to open <strong>Modal</strong>
+			</p>
 
-			<div className='mb-8 flex flex-wrap justify-center gap-8 bg-gray-300 p-4'>
+			<div className='mb-0 flex flex-wrap justify-center gap-8 bg-gray-300 p-4'>
 				<Figure
 					caption='Beagle'
 					styles='max-w-sm m-auto'
@@ -54,6 +58,55 @@ const ModalTemplate = () => {
 					/>
 				)}
 			</div>
+			<Codeblock language='jsx'>
+				{`import Image from 'next/image'
+				
+import Modal from '@/components/modal'		
+import Figure from '@/components/figure'
+
+interface DataProps {
+	name: string
+	src: string
+}
+
+const [img, setImg] = useState<DataProps>({
+	name: 'Beagle',
+	src: 'https://images.dog.ceo/breeds/beagle/n02088364_6866.jpg',
+})
+
+const [modal, setModal] = useState(false)
+
+const handleClick = () => {
+	setModal(true)
+	document.body.style.overflow = 'hidden'
+}
+
+const closeModal = () => {
+	setModal(false)
+	document.body.style.overflow = ''
+}
+     
+<Figure
+	caption='Beagle'
+	styles='max-w-sm m-auto'
+>
+	<Image
+		fill
+		src='https://images.dog.ceo/breeds/beagle/n02088364_6866.jpg'
+		title='Beagle'
+		alt='Beagle'
+		className='click'
+		onClick={handleClick}
+	/>
+</Figure>
+
+{modal && (
+	<Modal
+		data={img}
+		onClick={closeModal}
+	/>
+)}`}
+			</Codeblock>
 		</>
 	)
 }
