14e44716548aa9566c79ed0209ee12bb6968a96c
sjmitch-git <sjmitch20@outlook.com>
2023-06-14 07:52:55 +0100

    getPosts

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
-'use client'
-
-import { useState, useEffect } from 'react'
 import Link from 'next/link'
 
-// import { useFetch, FetchProps } from '@/hooks/usefetch'
 import { dehydrate, QueryClient } from '@tanstack/react-query'
 import PostList from '@/components/postlist'
 import { fetchPosts } from '@/hooks/usePosts'
-
-interface DataProps {
-	title: string
-	id: string
-}
 
 export default function Home() {
 	return (
