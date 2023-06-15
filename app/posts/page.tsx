0422bfa414c98e3b7bb94a9394eaf2dfa05b9176
sjmitch-git <sjmitch20@outlook.com>
2023-06-15 14:19:35 +0100

    getPosts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,18 +1,26 @@
-/* import ky from 'ky-universal'
+// import ky from 'ky-universal'
 
 import getQueryClient from '@/utils/getQueryClient'
 import Hydrate from '@/utils/hydrate.client'
 import { dehydrate } from '@tanstack/query-core'
 import Client from './client'
+import { Post } from '@/types/post'
 
-async function fetchPosts(limit: number) {
+/* async function fetchPosts(limit: number) {
 	const parsed: any[] = await ky('https://jsonplaceholder.typicode.com/posts').json()
 	return parsed.filter((x: { id: number }) => x.id <= limit)
+} */
+
+async function fetchPosts() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+	const users = (await res.json()) as Post[]
+	return users
 }
-/*  
-export default async function Hydation() {
+
+export default async function Hydration() {
 	const queryClient = getQueryClient()
-	await queryClient.prefetchQuery(['posts', 10], () => fetchPosts(10))
+	// await queryClient.prefetchQuery(['posts', 10], () => fetchPosts(10))
+	await queryClient.prefetchQuery(['posts', 10], fetchPosts)
 	const dehydratedState = dehydrate(queryClient)
 
 	return (
@@ -20,12 +28,4 @@ export default async function Hydation() {
 			<Client />
 		</Hydrate>
 	)
-} */
-
-export default async function Hydration() {
-	/* 	const queryClient = getQueryClient()
-	await queryClient.prefetchQuery(['posts', 10], () => fetchPosts(10))
-	const dehydratedState = dehydrate(queryClient) */
-
-	return <p>TEST</p>
 }
