1d338f67d001e1c86df7da5ac1c0efee951569de
sjmitch-git <sjmitch20@outlook.com>
2023-06-16 10:33:33 +0100

    posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,4 +1,4 @@
-import ky from 'ky-universal'
+// import ky from 'ky-universal'
 
 import getQueryClient from '@/utils/getQueryClient'
 import Hydrate from '@/utils/hydrate.client'
@@ -6,14 +6,20 @@ import { dehydrate } from '@tanstack/query-core'
 import ClientPosts from './client-posts'
 import { Post } from '@/types/post'
 
-async function fetchPosts(limit: number) {
+/* async function fetchPosts(limit: number) {
 	const parsed: Post[] = await ky('https://jsonplaceholder.typicode.com/posts').json()
 	return parsed.filter((x: { id: number }) => x.id <= limit) as Post[]
+} */
+
+async function fetchPosts() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+	const users = (await res.json()) as Post[]
+	return users
 }
 
 export default async function Hydration() {
 	const queryClient = getQueryClient()
-	await queryClient.prefetchQuery(['posts', 10], () => fetchPosts(10))
+	await queryClient.prefetchQuery(['posts'], fetchPosts)
 	const dehydratedState = dehydrate(queryClient)
 
 	return (
