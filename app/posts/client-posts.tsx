0c1342e903db0d320f777d7b98d1019da330c57a
sjmitch-git <sjmitch20@outlook.com>
2023-06-16 11:12:34 +0100

    usePosts

--- app/posts/client-posts.tsx
+++ app/posts/client-posts.tsx
@@ -1,31 +1,31 @@
 'use client'
 
-import React, { useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
+//import React, { useState } from 'react'
+// import { useQuery } from '@tanstack/react-query'
 // import ky from 'ky-universal'
-import { Post } from '@/types/post'
-// import { usePosts } from '@/hooks/usePosts'
+// import { Post } from '@/types/post'
+import { usePosts } from '@/hooks/usePosts'
 
 /* async function fetchPosts(limit: number) {
 	const parsed: Post[] = await ky('https://jsonplaceholder.typicode.com/posts').json()
 	return parsed.filter((x: { id: number }) => x.id <= limit) as Post[]
 } */
 
-async function fetchPosts() {
+/* async function fetchPosts() {
 	const res = await fetch('https://jsonplaceholder.typicode.com/posts')
 	const users = (await res.json()) as Post[]
 	return users
-}
+} */
 
 export default function ClientPosts() {
-	const [limit, setLimit] = useState(10)
+	//const [limit, setLimit] = useState(10)
 
-	// const { data, isLoading, error } = usePosts(limit)
+	const { data, isLoading, error } = usePosts()
 
-	const { data, isLoading, error } = useQuery({
+	/* const { data, isLoading, error } = useQuery({
 		queryKey: ['posts'],
 		queryFn: () => fetchPosts(),
-	})
+	}) */
 
 	console.log(data)
 	return (
--- hooks/usePosts/usePosts.ts
+++ hooks/usePosts/usePosts.ts
@@ -1,16 +1,21 @@
-import ky from 'ky-universal'
 import { useQuery } from '@tanstack/react-query'
 import { Post } from '@/types/post'
 
-const fetchPosts = async (limit = 10) => {
+/* const fetchPosts = async (limit = 10) => {
 	const parsed: Post[] = await ky('https://jsonplaceholder.typicode.com/posts').json()
 	return parsed.filter((x: { id: number }) => x.id <= limit) as Post[]
+} */
+
+async function fetchPosts() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+	const posts = (await res.json()) as Post[]
+	return posts
 }
 
-const usePosts = (limit: number) => {
+const usePosts = () => {
 	return useQuery({
-		queryKey: ['posts', limit],
-		queryFn: () => fetchPosts(limit),
+		queryKey: ['posts'],
+		queryFn: () => fetchPosts(),
 	})
 }
 
