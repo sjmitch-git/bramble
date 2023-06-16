f9f3af30561e398af4a36d5db7f4960c77c5282c
sjmitch-git <sjmitch20@outlook.com>
2023-06-16 12:00:21 +0100

    hooks

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -5,17 +5,18 @@ import Hydrate from '@/utils/hydrate.client'
 import { dehydrate } from '@tanstack/query-core'
 import ClientPosts from './client-posts'
 import { Post } from '@/types/post'
+import { fetchPosts } from '@/hooks/usePosts'
 
 /* async function fetchPosts(limit: number) {
 	const parsed: Post[] = await ky('https://jsonplaceholder.typicode.com/posts').json()
 	return parsed.filter((x: { id: number }) => x.id <= limit) as Post[]
 } */
 
-async function fetchPosts() {
+/* async function fetchPosts() {
 	const res = await fetch('https://jsonplaceholder.typicode.com/posts')
 	const users = (await res.json()) as Post[]
 	return users
-}
+} */
 
 export default async function Hydration() {
 	const queryClient = getQueryClient()
