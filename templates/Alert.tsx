579d451842d67f6f57b949f99303db54392f1c1a
sjmitch-git <sjmitch20@outlook.com>
2023-06-18 13:00:17 +0100

    Alert body

--- templates/Alert.tsx
+++ templates/Alert.tsx
@@ -3,10 +3,15 @@
 import React, { useState } from 'react'
 
 import Alert from '@/components/alert'
+import Button from '@/components/button'
 import Codeblock from '@/components/codeblock'
 
 const AlertTemplate = () => {
 	const [error, setError] = useState('Page could not be found!')
+
+	const handleClick = () => {
+		alert('You clicked me!')
+	}
 
 	return (
 		<>
@@ -18,7 +23,7 @@ const AlertTemplate = () => {
 <Alert
     styles='info' // 'info' | 'success' | 'warning' | 'error' | 'danger'
 	status='info'
-	message='Some contextual message useful to the user.'
+	message='Some contextual message useful to the user.' // can be HTML content
 />`}
 				</Codeblock>
 			</div>
@@ -27,7 +32,7 @@ const AlertTemplate = () => {
 				<Alert
 					styles='info'
 					status='Info'
-					message='Some contextual message useful to the user.'
+					message="<p><strong>Ugh umami</strong> butcher <a href='#'>pickled kogi</a> stumptown, <em>subway tile</em> gatekeep flexitarian. Bespoke meggings health goth meh ascot listicle, cronut vaporware. Marfa church-key cred, kinfolk mlkshk tousled roof party XOXO readymade solarpunk tbh adaptogen. Raclette williamsburg chicharrones, +1 retro hashtag freegan sustainable tumblr polaroid deep v tote bag godard.</p>"
 				/>
 			</div>
 
@@ -35,7 +40,7 @@ const AlertTemplate = () => {
 				<Alert
 					styles='success'
 					status='Success'
-					message='Some contextual message useful to the user.'
+					message='<p>Raclette williamsburg chicharrones, +1 retro hashtag freegan sustainable tumblr polaroid deep v tote bag godard<p>'
 				/>
 			</div>
 
